c3ce4887236e68c4b5d02a9af212c38923902c80
georgeee <george.agapov@gmail.com>
2016-03-13 22:39:30 +0300

    Few changes; added Frege

--- src/main/java/ru/georgeee/bachelor/yarn/graph/Node.java
+++ src/main/java/ru/georgeee/bachelor/yarn/graph/Node.java
@@ -2,12 +2,17 @@ package ru.georgeee.bachelor.yarn.graph;
 
 import lombok.Getter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 public class Node <T, V>{
-    private List<Edge> edges;
-    private T data;
+    private final List<Edge> edges = new ArrayList<>();
+    private final T data;
+
+    public Node(T data) {
+        this.data = data;
+    }
 
     @Getter
     public class Edge{

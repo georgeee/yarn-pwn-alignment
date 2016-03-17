c14d6289ad7eb74fd52022aaec9ed7ad2616648d
georgeee <george.agapov@gmail.com>
2016-03-17 05:21:51 +0300

    Small update in graphviz exporter

--- base/src/main/java/ru/georgeee/bachelor/yarn/graph/GraphVizHelper.java
+++ base/src/main/java/ru/georgeee/bachelor/yarn/graph/GraphVizHelper.java
@@ -9,11 +9,11 @@ import java.util.Map;
 @Component
 public class GraphVizHelper {
     public <T, V> void toGraph(NodeRepository<T, V> left, NodeRepository<V, T> right, Appendable out) throws IOException {
-        out.append("digraph synsets{\n layout fdp; ");
+        out.append("digraph synsets{\n layout = fdp;\n");
         addNodeDefs(true, left.getNodes(), out);
         addNodeDefs(false, right.getNodes(), out);
         addEdgeDefs(true, left.getNodes(), out);
-        addEdgeDefs(false, left.getNodes(), out);
+        addEdgeDefs(false, right.getNodes(), out);
         out.append("}");
     }
 
@@ -23,29 +23,33 @@ public class GraphVizHelper {
                 SynsetNode<V, T> to = e.getKey();
                 double weight = e.getValue();
                 out.append(isLeft ? "L_" : "R_")
-                        .append(from.getId())
+                        .append(gvEscapeId(from.getId()))
                         .append(" -> ")
                         .append(isLeft ? "R_" : "L_")
-                        .append(to.getId())
+                        .append(gvEscapeId(to.getId()))
                         .append(" [color=grey")
-                        .append(String.valueOf((int) (weight * 100)))
-                        .append(", label=")
+                        .append(String.valueOf((int) ((1 - weight) * 100)))
+                        .append(", label=\"")
                         .append(String.valueOf(weight))
-                        .append("];");
+                        .append("\"];\n");
             }
         }
+    }
+
+    private String gvEscapeId(String id){
+        return id.replace('-', '_');
     }
 
     private <T, V> void addNodeDefs(boolean isLeft, Iterable<SynsetNode<T, V>> nodes, Appendable out) throws IOException {
         for (SynsetNode<T, V> node : nodes) {
             out.append(isLeft ? "L_" : "R_")
-                    .append(node.getId())
-                    .append(" [label=")
+                    .append(gvEscapeId(node.getId()))
+                    .append(" [label=\"")
                     .append(isLeft ? "L " : "R ")
                     .append(node.getId())
-                    .append(", color=")
+                    .append("\", color=")
                     .append(isLeft ? "yellow" : "blue")
-                    .append("];");
+                    .append("];\n");
         }
     }
 
